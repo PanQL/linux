f2a8481ea74433df1b517a88964a69bec566e4be
Qinglin Pan <panqinglin2020@iscas.ac.cn>
2022-04-11 22:15:36 +0800

    mm: support Svnapot in huge vmap
    The HAVE_ARCH_HUGE_VMAP option can be used to help implement arch
    special huge vmap size. This patch selects this option by default and
    re-writes the arch_vmap_pte_range_map_size for Svnapot 64KB size.
    
    It can be tested when booting kernel in qemu with pci device, which
    will make the kernel to call pci driver using ioremap, and the
    re-written function will be called.
    
    Signed-off-by: Qinglin Pan <panqinglin2020@iscas.ac.cn>

--- arch/riscv/include/asm/vmalloc.h
+++ arch/riscv/include/asm/vmalloc.h
@@ -1,4 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 #ifndef _ASM_RISCV_VMALLOC_H
 #define _ASM_RISCV_VMALLOC_H
+
+#include <asm/pgtable.h>
+
+#ifdef CONFIG_SVNAPOT
+#define arch_vmap_pte_range_map_size arch_vmap_pte_range_map_size
+static inline unsigned long arch_vmap_pte_range_map_size(unsigned long addr, unsigned long end,
+		u64 pfn, unsigned int max_page_shift)
+{
+	bool is_napot_addr = !(addr & NAPOT_CONT64KB_MASK);
+	bool pfn_align_napot = !(pfn & (NAPOT_64KB_PTE_NUM - 1UL));
+	bool space_enough = ((end - addr) >= NAPOT_CONT64KB_SIZE);
+
+	if (has_svnapot() && is_napot_addr && pfn_align_napot && space_enough
+			&& max_page_shift >= NAPOT_CONT64KB_SHIFT)
+		return NAPOT_CONT64KB_SIZE;
+
+	return PAGE_SIZE;
+}
+#endif /*CONFIG_SVNAPOT*/
 
 #endif /* _ASM_RISCV_VMALLOC_H */
